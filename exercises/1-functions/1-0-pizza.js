15a02a1a9b6b707ed782d0f67dc6aca33dcf6c0d
MarijkeVDE <marijke.van.den.eede@student.ehb.be>
2023-08-08 12:06:32 +0200

    drawPizza fase1 index

--- exercises/1-functions/1-0-pizza.js
+++ exercises/1-functions/1-0-pizza.js
@@ -1,1 +1,20 @@
-"use strict";
+    "use strict";
+drawPizza()
+
+function drawPizza(){
+    //get canvas context
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2D");
+
+    // draw pizza with crust
+    context.fillStyle = "orange";
+    context.fillStroke = "brown";
+    context.lineWidth = 30;
+    context.beginPath();
+    context.arc(250, 250, Math.PI, Math.PI * 3 / 2);
+    context.stroke();
+    context.lineTo(250, 250);
+    context.fill();
+}
